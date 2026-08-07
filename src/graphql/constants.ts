cd85940587fd729a30c30c23c5b301b0b8fbed1e
Walker Mosakowski <billionaireyt2@gmail.com>
2026-08-07 09:59:56 -0400

    remove old nameing convention in favor of V2

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -489,34 +489,34 @@ export const graphUrls: Record<SupportedChainId, dexGraph> = {
     },
   },
   [SupportedChainId.hedera]: {
-    [SupportedDex.SaucerSwap]: {
+    [SupportedDex.SaucerSwapV2]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-saucerswap-1.2/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-saucerswap-1.2/gn',
       supportsCollectFees: true,
       version: 2,
     },
-    [SupportedDex.Bonzo]: {
+    [SupportedDex.BonzoV2]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-bonzo-1.2/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-bonzo-1.2/gn',
       supportsCollectFees: true,
       version: 2,
     },
-    [SupportedDex.Aux]: {
+    [SupportedDex.AuxV2]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-aux-1.2/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-aux-1.2/gn',
       supportsCollectFees: true,
       version: 2,
     },
-    [SupportedDex.SaucerSwap_Old]: {
+    [SupportedDex.SaucerSwap]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v1-saucerswap/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v1-saucerswap/gn',
       supportsCollectFees: true,
     },
-    [SupportedDex.Bonzo_Old]: {
+    [SupportedDex.Bonzo]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-bonzo/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-bonzo/gn',
@@ -524,7 +524,7 @@ export const graphUrls: Record<SupportedChainId, dexGraph> = {
       version: 2,
       supportsExtendedFeeAprs: true,
     },
-    [SupportedDex.Aux_Old]: {
+    [SupportedDex.Aux]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-aux/gn',
       publishedUrl:
         'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/g2/hedera-v2-aux/gn',
