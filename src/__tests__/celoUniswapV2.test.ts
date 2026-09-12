e32b65b7227ff16368e44bd9bc2b500fae6c7815
Walker Mosakowski <billionaireyt2@gmail.com>
2026-09-12 17:23:08 -0400

    Limit Celo deployment PR to configuration

--- src/__tests__/celoUniswapV2.test.ts
+++ src/__tests__/celoUniswapV2.test.ts
@@ -1,30 +1,18 @@
 /* eslint-env jest */
 
-import { getVaultsByPool, getVaultsByTokens } from '../functions/vault';
-import { graphqlRequest } from '../graphql/functions';
 import { SupportedChainId, SupportedDex } from '../types';
-import cache from '../utils/cache';
 import { getConfigByFactory } from '../utils/getConfigByFactory';
 import { getGraphUrls } from '../utils/getGraphUrls';
 
-jest.mock('../graphql/functions');
-
-const requestMock = graphqlRequest as jest.Mock;
 const chainId = SupportedChainId.celo;
 const endpoint = 'https://api.studio.thegraph.com/query/88584/celo-v-1-2/version/latest';
-const token0 = '0x0dc4f92879b7670e5f4e4e6e3c801d229129d90d';
-const token1 = '0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e';
-const pool = '0x5d8ef8b839be522b9e3d60a51edb5837cd0b2391';
 const originalApiKey = process.env.SUBGRAPH_API_KEY;
 
 beforeEach(() => {
-  cache.flushAll();
-  requestMock.mockReset();
   delete process.env.SUBGRAPH_API_KEY;
 });
 
 afterEach(() => {
-  cache.flushAll();
   if (originalApiKey === undefined) delete process.env.SUBGRAPH_API_KEY;
   else process.env.SUBGRAPH_API_KEY = originalApiKey;
 });
@@ -52,61 +40,5 @@ describe('Celo UniswapV2', () => {
       publishedUrl: apiKey ? endpoint : undefined,
       version: 2,
     });
-  });
-
-  it.each([SupportedDex.UniswapV3, SupportedDex.UniswapV2])(
-    'returns only vaults that allow the requested deposit token on %s',
-    async (dex) => {
-      const vaults = [
-        { id: 'token0-vault', tokenA: token0, tokenB: token1, allowTokenA: true, allowTokenB: false },
-        { id: 'token1-vault', tokenA: token0, tokenB: token1, allowTokenA: false, allowTokenB: true },
-      ];
-      const rawVaults =
-        dex === SupportedDex.UniswapV2
-          ? vaults.map((vault) => ({
-              id: vault.id,
-              token0: vault.tokenA,
-              token1: vault.tokenB,
-              allowToken0: vault.allowTokenA,
-              allowToken1: vault.allowTokenB,
-            }))
-          : vaults;
-      requestMock.mockImplementation(async (_url, _query, variables) => ({
-        ichiVaults: variables.addressTokenA === token0 ? rawVaults : [],
-      }));
-
-      expect(await getVaultsByTokens(chainId, dex, token0, token1)).toEqual([expect.objectContaining(vaults[0])]);
-      expect(await getVaultsByTokens(chainId, dex, token1, token0)).toEqual([expect.objectContaining(vaults[1])]);
-      expect(requestMock).toHaveBeenCalledTimes(2);
-    },
-  );
-
-  it('keeps token lookup caches separate for deployments on the same chain', async () => {
-    requestMock.mockImplementation(async (url, _query, variables) => ({
-      ichiVaults:
-        variables.addressTokenA === token0
-          ? [{ id: url, tokenA: token0, tokenB: token1, allowTokenA: true, allowTokenB: false }]
-          : [],
-    }));
-
-    const oldVaults = await getVaultsByTokens(chainId, SupportedDex.UniswapV3, token0, token1);
-    const newVaults = await getVaultsByTokens(chainId, SupportedDex.UniswapV2, token0, token1);
-    expect(newVaults[0].id).toBe(endpoint);
-    expect(newVaults).not.toEqual(oldVaults);
-    expect(await getVaultsByTokens(chainId, SupportedDex.UniswapV2, token0, token1)).toEqual(newVaults);
-    expect(await getVaultsByTokens(chainId, SupportedDex.UniswapV3, token0, token1)).toEqual(oldVaults);
-    expect(requestMock).toHaveBeenCalledTimes(4);
-  });
-
-  it('keeps pool lookup caches separate for deployments on the same chain', async () => {
-    requestMock.mockImplementation(async (url) => ({ deployICHIVaults: [{ vault: url }] }));
-
-    const oldVaults = await getVaultsByPool(pool, chainId, SupportedDex.UniswapV3);
-    const newVaults = await getVaultsByPool(pool, chainId, SupportedDex.UniswapV2);
-    expect(newVaults).toEqual([{ vault: endpoint }]);
-    expect(newVaults).not.toEqual(oldVaults);
-    expect(await getVaultsByPool(pool, chainId, SupportedDex.UniswapV2)).toEqual(newVaults);
-    expect(await getVaultsByPool(pool, chainId, SupportedDex.UniswapV3)).toEqual(oldVaults);
-    expect(requestMock).toHaveBeenCalledTimes(2);
   });
 });
